075e35761d267ab2075b69958cdca984facc5902
Adil Ahmad <adilahmad0025@gmail.com>
2024-06-04 14:53:13 +0530

    commit

---
+++ while_loop.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+
+    // while loop
+    // int i = 1;
+    // while(i <= 5){
+    //     cout << "Striver " << i << endl;
+    //     i = i+1;
+    // }
+
+    // Do while loop
+    int j = 2;
+    do{
+        cout << "Striver " << j << endl;
+        j = j+1;
+    } while(j <= 1);
+    cout << j << endl;
+
+
+
+    return 0;
+}
