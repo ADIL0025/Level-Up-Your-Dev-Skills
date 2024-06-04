651e25cd30c67f00e35d120c33deba84bb106de2
Adil Ahmad <adilahmad0025@gmail.com>
2024-06-04 14:44:02 +0530

    first commit

---
+++ basic_arr_str.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    // 1D array
+    // int arr[5];
+    // cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4];
+
+    // arr[3] += 10;
+    // arr[4] = 10;
+    // cout << arr[3] << endl;
+    // cout << arr[4];
+
+
+    // 2D array
+    // int arr[3][5] = 55;
+    // cout << arr[3][5];
+
+
+    // basic string
+    string s = "Striver";
+    cout << s[3] << endl;
+    int len = s.size();
+    s[len-1] = 'z';
+    cout << s[len-1] << endl;
+    cout << len;
+    
+    
+    
+    return 0;
+}
---
+++ for_loop.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main (){
+    // int i;
+    // for(i = 1; i <= 10; i++){
+    //     cout << "Striver" << endl;
+    // }
+    // cout << i << endl;
+
+    int i ;
+    for (i = 5; i > 0; i = i-1){
+        cout << "Striver" << i << endl;
+    }
+    cout << i << endl;
+
+
+
+    return 0;
+}
