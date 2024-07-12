062007d7b1d0975fb702ede5b7230dae4104c0c2
Adil Ahmad <adilahmad0025@gmail.com>
2024-07-12 11:39:56 +0530

    commit

---
+++ Recursion/rec_1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+void print() {
+    cout << 1 << endl;
+    print();
+}
+
+int main(){
+    print();
+} */ 
+
+// Recursion with specfic conditions
+
+int cnt = 0;
+void print(){
+    if(cnt == 4){
+        return;
+    }
+    cout << cnt << endl;
+    cnt++;
+    print();
+} 
+
+int main(){
+    print();
+}
