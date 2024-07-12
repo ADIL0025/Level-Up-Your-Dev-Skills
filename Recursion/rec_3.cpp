f967e3d579faa22e94645f7582e6d1a0b1a5138e
Adil Ahmad <adilahmad0025@gmail.com>
2024-07-12 18:48:30 +0530

    commit

---
+++ Recursion/rec_3.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Sum of N numbers(N to 1)
+// parameterized
+/*void func(int i, int sum){
+    if(i < 1){
+        cout<< sum << endl;
+        return;
+    }
+    func(i-1,sum+i);
+}
+
+int main(){
+    int n;
+    cin >> n ;
+    func(n,0);
+    return 0;
+}*/
+
+// functional recursion
+/*int sum(int n)
+{
+    if(n == 0) return 0;
+    return n + sum(n-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << sum(n);
+    return 0;
+}*/
+
+// Factorial of a number
+
+int fact(int n){
+    if(n == 0) return 1;
+    return n * fact(n-1);
+}
+
+int main(){
+    int n;
+    cin >> n;
+    cout << fact(n);
+    return 0;
+}
+
