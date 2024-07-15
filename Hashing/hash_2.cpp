d901d70864e923e5ec118d7990089a850dc5baaf
Adil Ahmad <adilahmad0025@gmail.com>
2024-07-15 20:21:33 +0530

    commit

---
+++ Hashing/hash_2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// For Characters hashing
+
+int main(){
+    string s;
+    cin >> s;
+
+    // Precompute
+    int hash[26] = {0}; // if we dont know the index value of characters we simply take the 'size = 256' because it contains all the elements. 
+    // int hash[256] = {0};
+    for (int i = 0; i < s.size(); i++){
+        hash[s[i]-'a']++; // hash[s[i]]++;
+    }
+
+    int q;
+    cin >> q;
+    while(q--){
+        char c;
+        cin >> c;
+        // fetch
+        cout << hash[c-'a'] << endl;
+    }
+    
+}
---
+++ Hashing/hash_map.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// using Mapping techniques
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+
+    // Pre-compute
+    map<int , int> mpp;
+    for (int i = 0; i < n; i++)
+    {
+        mpp[arr[i]]++;
+    }
+
+    // iterate in the map
+    // for(auto it : mpp){
+    //     cout << it.first << "->" << it.second << endl;
+    // }
+
+    int q;
+    cin >> q;
+    while(q--){
+        int number;
+        cin >> number;
+        // fetch
+        cout << mpp[number] << endl;
+    }
+    return 0;
+}
