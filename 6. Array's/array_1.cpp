2d4efec534416e373f5034b424c83dcec494b7a8
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-20 12:13:05 +0530

    commit

---
+++ 6. Array's/array_1.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------------------------- find the largest element in the array
+// int findMax(vector<int> &arr, int n) {
+//     int max = arr[0];
+//     for (int i = 1; i < n; i++) {
+//         if (arr[i] > max) {
+//             max = arr[i];
+//         }
+//     }
+//     return max;
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     cout << findMax(arr, n) << endl;
+// }
+
+// ---------------------------- find the second largest element in the array
