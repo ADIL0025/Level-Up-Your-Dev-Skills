fa85a2250f01b69487f7ac265d6653a6ce16713e
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-20 12:35:50 +0530

    commit

--- 6. Array's/array_1.cpp
+++ 6. Array's/array_1.cpp
@@ -23,3 +23,43 @@ using namespace std;
 // }
 
 // ---------------------------- find the second largest element in the array
+int secondlargest(vector<int> &a, int n){
+    int largest = a[0];
+    int slargest =  -1;
+    for(int i = 0; i < n; i++){
+        if(a[i] > largest){
+            slargest = largest;
+            largest = a[i];
+        }
+        else if (a[i] < largest && a[i] > slargest){
+            slargest = a[i];
+        }
+    }
+    return slargest;
+}
+
+int secondsmallest(vector<int> &a, int n){
+    int smallest = a[0];
+    int ssmallest = INT_MAX;
+    for(int i = 0; i < n; i++){
+        if(a[i] < smallest){
+            ssmallest = smallest;
+            smallest = a[i];
+        }
+        else if (a[i] != smallest && a[i] < ssmallest){
+            ssmallest = a[i];
+        }
+    }
+    return ssmallest;
+}
+
+int main(){
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Second Largest Element is: " <<secondlargest(arr, n) << endl;
+    cout << "Second Smallest Element is: " <<secondsmallest(arr, n) << endl;
+}
