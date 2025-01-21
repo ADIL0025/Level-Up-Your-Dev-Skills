95d78d05919a4b384ebb66440d8950e9bd19b58f
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-21 12:39:08 +0530

    commit

--- 6. Array's/array_2.cpp
+++ 6. Array's/array_2.cpp
@@ -27,32 +27,77 @@ using namespace std;
 
 // ---------------------------- Left Rotate an array by d places
 
-void leftRotate(vector<int> &arr, int n, int d){
-    d = d % n;
-    vector<int> temp(d);
-    for(int i = 0; i < d; i++){
-        temp[i] = arr[i];
-    }
-    for(int i = d; i < n; i++){
-        arr[i-d] = arr[i];
-    }
-    for(int i = n-d; i < n; i++){
-        arr[i] = temp[i-(n-d)];
-    }
-}
+// void leftRotate(vector<int> &arr, int n, int d){
+//     d = d % n;
+//    // ------ Brute Force Approach
+//     vector<int> temp(d);
+//     for(int i = 0; i < d; i++){
+//         temp[i] = arr[i];
+//     }
+//     for(int i = d; i < n; i++){
+//         arr[i-d] = arr[i];
+//     }
+//     for(int i = n-d; i < n; i++){
+//         arr[i] = temp[i-(n-d)];
+//     }
 
-int main(){
-    int n;
-    cin >> n;
-    vector<int> arr(n);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
-    int d;
-    cin >> d;
-    leftRotate(arr, n, d);
-    for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-    return 0;
-}
+//     // ------ Optimal Approach
+//     reverse(arr.begin(), arr.begin() + d);
+//     reverse(arr.begin() + d, arr.end());
+//     reverse(arr.begin(), arr.end());
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     int d;
+//     cin >> d;
+//     leftRotate(arr, n, d);
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// ---------------------------- Move all zero's to the end of the array
+
+
+// ------------ Brute force approach
+// vector<int> moveZeroes(vector<int> &arr, int n){
+//     vector<int> temp;
+//     for(int i =0; i <n; i++){
+//         if(arr[i] != 0){
+//             temp.push_back(arr[i]);
+//         }
+//     }
+//     int nz = temp.size();
+//     for(int i = 0; i < nz; i++){
+//         arr[i] = temp[i];
+//     }
+//     for(int i = nz; i < n; i++){
+//         arr[i] = 0;
+//     }
+//     return arr;
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     moveZeroes(arr, n);
+//     for (int i = 0; i < n; i++) {
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+// ------------ Optimal approach (Two pointer approach)
+vector<int> moveZeroes(vector<int> &arr, int n){}
