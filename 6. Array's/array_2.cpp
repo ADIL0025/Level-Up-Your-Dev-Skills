b1fd85b78ec98730c352e3fb11d4f4895f001b4f
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-21 12:43:20 +0530

    commit

--- 6. Array's/array_2.cpp
+++ 6. Array's/array_2.cpp
@@ -100,4 +100,36 @@ using namespace std;
 // }
 
 // ------------ Optimal approach (Two pointer approach)
-vector<int> moveZeroes(vector<int> &arr, int n){}
+vector<int> moveZeroes(vector<int> &arr, int n){
+    int j = -1;
+    for(int i = 0; i < n; i++){
+        if(arr[i] == 0){
+            j = i;
+            break;
+        }
+    }
+    if(j == -1){
+        return arr;
+    }
+    for(int i = j+1; i < n; i++){
+        if(arr[i] != 0){
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+    return arr;
+}
+
+int main(){
+    int n; 
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    moveZeroes(arr, n);
+    for(int i = 0; i < n; i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
