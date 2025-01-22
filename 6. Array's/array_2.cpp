b1d2a74da08d16d23af1b4a95449c2bb4292d1d7
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-22 13:17:55 +0530

    Refactor array operations: remove unused functions and implement intersection logic

--- 6. Array's/array_2.cpp
+++ 6. Array's/array_2.cpp
@@ -100,36 +100,167 @@ using namespace std;
 // }
 
 // ------------ Optimal approach (Two pointer approach)
-vector<int> moveZeroes(vector<int> &arr, int n){
-    int j = -1;
-    for(int i = 0; i < n; i++){
-        if(arr[i] == 0){
-            j = i;
-            break;
-        }
-    }
-    if(j == -1){
-        return arr;
-    }
-    for(int i = j+1; i < n; i++){
-        if(arr[i] != 0){
-            swap(arr[i], arr[j]);
-            j++;
-        }
-    }
-    return arr;
+// vector<int> moveZeroes(vector<int> &arr, int n){
+//     int j = -1;
+//     for(int i = 0; i < n; i++){
+//         if(arr[i] == 0){
+//             j = i;
+//             break;
+//         }
+//     }
+//     if(j == -1){
+//         return arr;
+//     }
+//     for(int i = j+1; i < n; i++){
+//         if(arr[i] != 0){
+//             swap(arr[i], arr[j]);
+//             j++;
+//         }
+//     }
+//     return arr;
+// }
+
+// int main(){
+//     int n; 
+//     cin >> n;
+//     vector<int> arr(n);
+//     for(int i = 0; i < n; i++){
+//         cin >> arr[i];
+//     }
+//     moveZeroes(arr, n);
+//     for(int i = 0; i < n; i++){
+//         cout << arr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// ------------------------------- Linear Search
+// int linearSearch(vector<int> &arr, int n, int num){
+//     for(int i = 0; i < n; i++){
+//         if(arr[i] == num){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for(int i = 0; i < n; i++){
+//         cin >> arr[i];
+//     }
+//     int num;
+//     cin >> num;
+//     cout << linearSearch(arr, n, num);
+//     return 0;
+// }
+
+// ------------------------------- Union of two arrays
+// vector < int > sortedArray(vector < int > a, vector < int > b) {
+
+//     int n1 = a.size();
+//     int n2 = b.size();
+//     int i = 0;
+//     int j = 0;
+
+//     vector<int> unionArr;
+
+//     while(i < n1 && j <n2){
+//         if(a[i] <= b[j]){
+//             if(unionArr.size() == 0 || unionArr.back() != a[i]){
+//                 unionArr.push_back(a[i]);
+//             }
+//             i++;
+//         }
+
+//         else{
+//             if(unionArr.size() == 0 || unionArr.back() != b[j]){
+//                 unionArr.push_back(b[j]);
+//             }
+//             j++;
+//         }
+//     }
+
+//     while(j < n2){
+//         if (unionArr.size() == 0 || unionArr.back() != b[j]) {
+//                 unionArr.push_back(b[j]);
+//             }
+//             j++;     
+//         }
+
+//     while(i < n1){
+//         if (unionArr.size() == 0 || unionArr.back() != a[i]) {
+//                 unionArr.push_back(a[i]);
+//             }
+//             i++;     
+//         }
+//         return unionArr;
+
+// }
+
+// int main(){
+//     int n1;
+//     cin >> n1;
+//     vector<int> a(n1);
+//     for(int i = 0; i < n1; i++){
+//         cin >> a[i];
+//     }
+//     int n2;
+//     cin >> n2;
+//     vector<int> b(n2);
+//     for(int i = 0; i < n2; i++){
+//         cin >> b[i];
+//     }
+//     vector<int> unionArr = sortedArray(a, b);
+//     for(int i = 0; i < unionArr.size(); i++){
+//         cout << unionArr[i] << " ";
+//     }
+//     return 0;
+// }
+
+
+// ------------------------------- Intersection of two arrays
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+
+	int i = 0;
+	int j = 0;
+	vector<int> ans;
+	while(i < n && j < m){
+		if(arr1[i] < arr2[j]){
+			i++;
+		}
+		else if(arr2[j] < arr1[i]){
+			j++;
+		}
+		else{
+			ans.push_back(arr1[i]);
+			i++;
+			j++;
+		}
+	}
+	return ans;
 }
 
 int main(){
-    int n; 
+    int n;
     cin >> n;
-    vector<int> arr(n);
+    vector<int> arr1(n);
     for(int i = 0; i < n; i++){
-        cin >> arr[i];
+        cin >> arr1[i];
     }
-    moveZeroes(arr, n);
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
+    int m;
+    cin >> m;
+    vector<int> arr2(m);
+    for(int i = 0; i < m; i++){
+        cin >> arr2[i];
+    }
+    vector<int> ans = findArrayIntersection(arr1, n, arr2, m);
+    for(int i = 0; i < ans.size(); i++){
+        cout << ans[i] << " ";
     }
     return 0;
 }
