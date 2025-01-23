7763a0b0be2803c348308e5ea726960e2fcfbb9a
Adil Ahmad <adilahmad0025@gmail.com>
2025-01-23 11:37:25 +0530

    Implement function to find the maximum consecutive 1's in an array and remove the missing number function

--- 6. Array's/array_3.cpp
+++ 6. Array's/array_3.cpp
@@ -2,17 +2,44 @@
 using namespace std;
 
 // ---------------------------- find the missing no. in the array
-int missingNumber(vector<int>&a, int N) {
+// int missingNumber(vector<int>&a, int N) {
 
-    int xor1 = 0;
-    int xor2 = 0;
-    int n = N - 1;
-    for(int i = 0; i < n; i++){
-        xor2 = xor2 ^ a[i];
-        xor1 = xor1 ^ (i+1);
+//     int xor1 = 0;
+//     int xor2 = 0;
+//     int n = N - 1;
+//     for(int i = 0; i < n; i++){
+//         xor2 = xor2 ^ a[i];
+//         xor1 = xor1 ^ (i+1);
+//     }
+//     xor1 = xor1 ^ N;
+//     return xor1 ^ xor2;
+// }
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vector<int> arr(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+//     cout << missingNumber(arr, n) << endl;
+// }
+
+// ---------------------------- find the maximum consecutive 1's in an array
+int findMaxConsecutiveOnes(vector<int>& nums) {
+    int maxi = 0;
+    int cnt = 0;
+
+    for(int i = 0; i < nums.size(); i++){
+        if(nums[i] == 1){
+            cnt++;
+            maxi = max(maxi, cnt);
+        }
+        else{
+            cnt = 0;
+        }
     }
-    xor1 = xor1 ^ N;
-    return xor1 ^ xor2;
+    return maxi;
 }
 
 int main(){
@@ -22,5 +49,5 @@ int main(){
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    cout << missingNumber(arr, n) << endl;
+    cout << findMaxConsecutiveOnes(arr) << endl;
 }
